1225c1a9c84bd2f9058f05e2e7b8468ede87be18
Kladov Eugene <kladov-eugene@ac-sw.com>
2017-03-26 20:55:12 +0700

    Web Stack calculator on Spring Boot, initial commit.

---
+++ kladov.evgeny/task2a/src/main/java/com/example/Task2aApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Task2aApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Task2aApplication.class, args);
+	}
+}
